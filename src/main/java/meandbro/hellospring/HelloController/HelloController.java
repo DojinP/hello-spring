0d5da86362c133e502f05ffe74f0bff5ecdaaf4d
Dojin Park <coah2013@naver.com>
2023-10-24 13:51:29 +0900

    Section 2-2

--- src/main/java/meandbro/hellospring/HelloController/HelloController.java
+++ src/main/java/meandbro/hellospring/HelloController/HelloController.java
@@ -3,6 +3,7 @@ package meandbro.hellospring.HelloController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -13,5 +14,11 @@ public class HelloController {
         // Controller 에서 return 되는 문자열은 viewResolver 를 통해 "resources:templates/{ViewName}.html"의 형식으로 매핑 됨
         // (ViewName = return 되는 문자열)
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
