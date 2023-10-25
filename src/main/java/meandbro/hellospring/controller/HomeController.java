ecda5a5bb59aecb558c0c54d57a489ce6409da8f
Dojin Park <coah2013@naver.com>
2023-10-25 15:20:49 +0900

    Section 5-2
    회원기능 - 등록

--- src/main/java/meandbro/hellospring/controller/HomeController.java
+++ src/main/java/meandbro/hellospring/controller/HomeController.java
@@ -1,14 +1,41 @@
 package meandbro.hellospring.controller;
 
+import meandbro.hellospring.domain.Member;
+import meandbro.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
+    private MemberService memberService;
+
+    @Autowired
+    public HomeController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "home";
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 
 }
