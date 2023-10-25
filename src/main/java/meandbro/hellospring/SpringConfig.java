704815ecd6750f2c2fe091745bdee5ddf6dc46ef
Dojin Park <coah2013@naver.com>
2023-10-25 17:16:52 +0900

    Section 6-3
    순수 JDBC 구현

--- src/main/java/meandbro/hellospring/SpringConfig.java
+++ src/main/java/meandbro/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package meandbro.hellospring;
 
+import meandbro.hellospring.repository.JdbcMemberRepository;
 import meandbro.hellospring.repository.MemberRepository;
 import meandbro.hellospring.repository.MemoryMemberRepository;
 import meandbro.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
