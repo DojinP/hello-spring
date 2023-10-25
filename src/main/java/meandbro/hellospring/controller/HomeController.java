0cdadaf5243d5473b7faa8cb7d437eb1d45ee410
Dojin Park <coah2013@naver.com>
2023-10-25 15:26:15 +0900

    Section 5-3
    회원기능 - 조회

--- src/main/java/meandbro/hellospring/controller/HomeController.java
+++ src/main/java/meandbro/hellospring/controller/HomeController.java
@@ -4,9 +4,13 @@ import meandbro.hellospring.domain.Member;
 import meandbro.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -36,6 +40,14 @@ public class HomeController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 
 }
