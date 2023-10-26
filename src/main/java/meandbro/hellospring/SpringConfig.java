ce4ac502398c6f05e574c7d94f80066daa60b4cc
Dojin Park <coah2013@naver.com>
2023-10-26 14:09:44 +0900

    Section 7
    AOP 적용

--- src/main/java/meandbro/hellospring/SpringConfig.java
+++ src/main/java/meandbro/hellospring/SpringConfig.java
@@ -1,28 +1,16 @@
 package meandbro.hellospring;
 
+import meandbro.hellospring.aop.TimeTraceAop;
 import meandbro.hellospring.repository.*;
 import meandbro.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
-//    private DataSource dataSource;
-//    private EntityManager em;
     private final MemberRepository memberRepository;
-
-    /*
-        public SpringConfig(DataSource dataSource) {
-            this.dataSource = dataSource;
-        }*/
-    /*public SpringConfig(EntityManager em) {
-        this.em = em;
-    }*/
     @Autowired
     public SpringConfig(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -35,10 +23,7 @@ public class SpringConfig {
     }
 
     /*@Bean
-    public MemberRepository memberRepository() {
-        //return new MemoryMemberRepository();
-        //return new JdbcMemberRepository(dataSource);
-        //return new JdbcTemplateMemberRepository(dataSource);
-        //return new JpaMemberRepository(em);
+    public TimeTraceAop timeTraceAop() {
+        return new TimeTraceAop();
     }*/
 }
